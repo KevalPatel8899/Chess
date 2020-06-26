c5050d9f8c82eb70db4c2bfb5ac3cda7c49648ac
Keval Patel <keval881999@gmail.com>
2020-06-26 12:57:51 -0400

    ChessPiece Class Creation

---
+++ app/src/main/java/com/example/chess/ChessPiece.java
@@ -0,0 +1,48 @@
+package com.example.chess;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class ChessPiece{
+
+    String chessPieceLocation, chessPiece;
+
+    private ChessPiece(String chessPieceLocation, String chessPiece){
+        chessPiece = this.chessPiece;
+        chessPieceLocation = this.chessPieceLocation;
+    }
+
+    public List<String>  chessMove(){
+        int location = Integer.parseInt(chessPieceLocation.substring(chessPieceLocation.length() - 2));
+        List<String> locations = new ArrayList<>();
+        int locationX = Integer.parseInt(chessPieceLocation.substring(chessPieceLocation.length() - 2, chessPieceLocation.length() - 1));
+        int locationY = Integer.parseInt(chessPieceLocation.substring(chessPieceLocation.length() - 1));
+        locations.clear();
+
+        switch (chessPiece.substring(8)){
+            case "Horse":{
+
+                if(locationX + 2 <= 7 && locationY + 1 <= 7)locations.add("box"+Integer.toString( location + 21));
+                if(locationX + 1 <= 7 && locationY + 2 <= 7)locations.add("box"+Integer.toString(location + 12));
+                if(locationX - 1 >= 0 && locationY - 2 >= 0){locations.add("box"+Integer.toString(location - 12));}
+                if(locationX - 2 >= 0 && locationY - 1 >= 0){locations.add("box"+Integer.toString(location - 21));}
+
+
+                if(locationX + 2 <= 7 && locationY - 1 >= 0)locations.add("box"+Integer.toString( locationX + 2) +Integer.toString(locationY - 1));
+                if(locationX + 1 <= 7 && locationY - 2 >= 0)locations.add("box"+Integer.toString( locationX + 1) +Integer.toString(locationY - 2));
+                if(locationX - 1 >= 0 && locationY + 2 <= 7){locations.add("box"+Integer.toString( locationX - 1) +Integer.toString(locationY + 2));}
+                if(locationX - 2 >= 0 && locationY + 1 <= 7){locations.add("box"+Integer.toString( locationX - 2) +Integer.toString(locationY + 1));}
+            }
+            break;
+
+            case "Bishop" : {
+
+            }
+            break;
+        }
+
+
+
+        return locations;
+    }
+}
