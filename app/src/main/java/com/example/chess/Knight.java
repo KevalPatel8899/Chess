be99805d85978ef51420c6a69eef9a4206a3f937
Keval Patel <keval881999@gmail.com>
2020-07-01 18:01:31 -0400

    Removal of extra classes

--- app/src/main/java/com/example/chess/Knight.java
+++
@@ -1,9 +0,0 @@
-package com.example.chess;
-
-public class Knight extends ChessPiece {
-
-    protected Knight(String chessPieceLocation, String chessPiece) {
-        super(chessPieceLocation, chessPiece);
-    }
-
-}
